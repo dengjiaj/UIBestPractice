481fadfcc14544e7eb217bb2a46fa37710cbc1f2
dengjiaj <465594616@qq.com>
2017-04-12 16:33:30 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,3 @@
+# UIBestPractice
+### 第一百行代码
+UI布局的简单应用----短信APP
---
+++ app/src/main/java/com/example/uibestpractice/Msg.java
@@ -0,0 +1,23 @@
+package com.example.uibestpractice;
+
+/**
+ * Created by Administrator on 2017/4/9.
+ */
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SEND = 1;
+    public String content;
+    private int type;
+    public Msg(String content,int type){
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
